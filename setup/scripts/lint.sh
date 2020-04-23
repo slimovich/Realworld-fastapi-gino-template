fa0d34f39fd408a3c16739227237bb9b0a5e3485
slim <slim.baccar91@gmail.com>
2020-04-23 17:00:49 +0200

    update setup

--- setup/scripts/lint.sh
+++ setup/scripts/lint.sh
@@ -2,6 +2,14 @@
 
 set -x
 
+FILE=reports/flake8.txt
+if [ -f "$FILE" ]; then
+    echo "$FILE exist"
+    rm reports/flake8.txt
+else 
+    echo "$FILE does not exist"
+fi
+
 flake8 src/ tests/
 
 mypy src/ tests/
