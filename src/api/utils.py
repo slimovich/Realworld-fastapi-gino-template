77375617fb541df9ca9b718b2f8cf0b4fd529d44
slim <slim.baccar91@gmail.com>
2020-06-29 21:32:04 +0200

    Fix linting issues

--- src/api/utils.py
+++ src/api/utils.py
@@ -2,5 +2,5 @@ from src.domain.userManagment.service.userService import UserService
 from src.infrastructure.database.models.user import UserQueries
 
 
-def get_user_services():
+def get_user_services() -> UserService:
     return UserService(UserQueries())
--- src/infrastructure/database/models/user.py
+++ src/infrastructure/database/models/user.py
@@ -5,7 +5,7 @@ from src.domain.userManagment.interface.queries import IUserQueries
 from src.domain.userManagment.schema.user import UserCreateSchema, UserUpdateSchema
 
 
-class UserModel(db.Model):  # type: ignore
+class UserModel(db.Model):
     __tablename__ = "user base"
 
     user_id = db.Column("id", db.Integer, autoincrement=True, primary_key=True)
