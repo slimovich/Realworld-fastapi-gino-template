c53c472e06aecf52831dc9f2f79d87641032ac82
slim <slim.baccar91@gmail.com>
2020-07-02 13:44:17 +0200

    Try fixing test

--- tests/api/test_user_router.py
+++ tests/api/test_user_router.py
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import Any, List
 
 import pytest
 from fastapi import FastAPI
@@ -34,6 +34,18 @@ USER_MODEL = UserModel(
 
 class UserServiceDummy:
     async def create_user(self, user: Any) -> UserModel:
+        return USER_MODEL
+
+    async def list_users(self) -> List[UserModel]:
+        return [USER_MODEL]
+
+    async def get_user_by_id(self, id: int) -> UserModel:
+        return USER_MODEL
+
+    async def update_user(self, id: int, new_user: Any) -> UserModel:
+        return USER_MODEL
+
+    async def remove_user(self, id: int) -> UserModel:
         return USER_MODEL
 
 
@@ -77,5 +89,46 @@ class TestUserRouter:
                 "created_date": "1/1/2020",
             }
         )
+        assert response.status_code == status.HTTP_200_OK
+        assert response.json() == user_schema.__dict__
+
+    def test_user_update(
+        self, user_model: UserModel, user_schema: UserDBSchema
+    ) -> None:
+
+        response = client.put(
+            "/users/1",
+            json={
+                "email": "test@test.com",
+                "full_name": "test",
+                "password": "test",
+                "is_active": True,
+                "is_superuser": False,
+                "created_date": "1/1/2020",
+            })
+        assert response.status_code == status.HTTP_200_OK
+        assert response.json() == user_schema.__dict__
+
+    def test_user_delete(
+        self, user_model: UserModel, user_schema: UserDBSchema
+    ) -> None:
+
+        response = client.delete("/users/1")
+        assert response.status_code == status.HTTP_200_OK
+        assert response.json() == user_schema.__dict__
+
+    def test_user_list_all(
+        self, user_model: UserModel, user_schema: UserDBSchema
+    ) -> None:
+
+        response = client.get("/users/all")
+        assert response.status_code == status.HTTP_200_OK
+        assert response.json() == [user_schema.__dict__]
+
+    def test_user_get_byid(
+        self, user_model: UserModel, user_schema: UserDBSchema
+    ) -> None:
+
+        response = client.get("/users/1")
         assert response.status_code == status.HTTP_200_OK
         assert response.json() == user_schema.__dict__
--- tests/domain/service/test_user_service.py
+++ tests/domain/service/test_user_service.py
@@ -1,8 +1,8 @@
-from typing import Any
+from typing import Any, List
 
 import pytest
 
-from src.domain.userManagment.schema.user import UserCreateSchema, UserDBSchema
+from src.domain.userManagment.schema.user import UserCreateSchema, UserDBSchema, UserUpdateSchema
 from src.domain.userManagment.service.userService import UserService
 from src.infrastructure.database.models.user import UserModel
 
@@ -20,6 +20,18 @@ USER_MODEL = UserModel(
 class UserQueriesDummy:
     async def create_user(self, user: Any) -> UserModel:
         return USER_MODEL
+
+    async def update_user(self, old_user: Any, new_user: Any) -> UserModel:
+        return USER_MODEL
+
+    async def delete_user(self, user_id: int) -> UserModel:
+        return USER_MODEL
+
+    async def get_user_byid(self, user_id: int) -> UserModel:
+        return USER_MODEL
+
+    async def get_all_users(self) -> List[UserModel]:
+        return [USER_MODEL]
 
 
 @pytest.fixture
@@ -40,6 +52,19 @@ def user_schema() -> UserCreateSchema:
     )
 
 
+@pytest.fixture
+def user_update_schema() -> UserUpdateSchema:
+    return UserUpdateSchema(
+        user_id=1,
+        email="test@test.com",
+        full_name="test",
+        password="test",
+        is_active=True,
+        is_superuser=False,
+        created_date="1/1/2020",
+    )
+
+
 class TestUserService:
     @pytest.mark.asyncio
     async def test_user_create_valide(
@@ -48,6 +73,70 @@ class TestUserService:
         user_service = UserService(UserQueriesDummy())
 
         result = await user_service.create_user(user_schema)
+        assert result == UserDBSchema(
+            user_id=1,
+            email="test@test.com",
+            full_name="test",
+            is_active=True,
+            is_superuser=False,
+            created_date="1/1/2020",
+        )
+
+    @pytest.mark.asyncio
+    async def test_user_list_users(
+        self, user_model: UserModel, user_schema: UserCreateSchema
+    ) -> None:
+        user_service = UserService(UserQueriesDummy())
+
+        result = await user_service.list_users()
+        assert result == [UserDBSchema(
+            user_id=1,
+            email="test@test.com",
+            full_name="test",
+            is_active=True,
+            is_superuser=False,
+            created_date="1/1/2020",
+        )]
+
+    @pytest.mark.asyncio
+    async def test_user_update_user(
+        self, user_model: UserModel, user_update_schema: UserUpdateSchema
+    ) -> None:
+        user_service = UserService(UserQueriesDummy())
+
+        result = await user_service.update_user(1, user_update_schema)
+        assert result == UserDBSchema(
+            user_id=1,
+            email="test@test.com",
+            full_name="test",
+            is_active=True,
+            is_superuser=False,
+            created_date="1/1/2020",
+        )
+
+    @pytest.mark.asyncio
+    async def test_user_remove_user(
+        self, user_model: UserModel, user_schema: UserCreateSchema
+    ) -> None:
+        user_service = UserService(UserQueriesDummy())
+
+        result = await user_service.remove_user(1)
+        assert result == UserDBSchema(
+            user_id=1,
+            email="test@test.com",
+            full_name="test",
+            is_active=True,
+            is_superuser=False,
+            created_date="1/1/2020",
+        )
+
+    @pytest.mark.asyncio
+    async def test_user_get_user_by_id(
+        self, user_model: UserModel, user_schema: UserCreateSchema
+    ) -> None:
+        user_service = UserService(UserQueriesDummy())
+
+        result = await user_service.get_user_by_id(1)
         assert result == UserDBSchema(
             user_id=1,
             email="test@test.com",
