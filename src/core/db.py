19dd5009462121167cb821facca337361d21003c
slim <slim.baccar91@gmail.com>
2020-07-02 16:37:33 +0200

    Fixing coverage

--- src/core/db.py
+++ src/core/db.py
@@ -7,9 +7,6 @@ from src.core.config import DATABASE_URI
 LOGGER = logging.getLogger(__name__)
 
 
-try:
-    # create Gino engine
-    LOGGER.info(f"Initialise Gino engine and connecting to {DATABASE_URI}")
-    db = Gino(dsn=DATABASE_URI)
-except Exception as e:
-    LOGGER.error(f"Error in Gino engine initialisation => {e}")
+# create Gino engine
+LOGGER.info(f"Initialise Gino engine and connecting to {DATABASE_URI}")
+db = Gino(dsn=DATABASE_URI)
